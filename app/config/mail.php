74e4eca40e5d524203412213efad6ef41da7ad9a
Phill Sparks <me@phills.me.uk>
2013-04-12 01:03:35 +0100

    Send a welcome email to each group that registers.

--- app/config/mail.php
+++ app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => null, 'name' => null),
+	'from' => array('address' => 'underground@womble.me.uk', 'name' => 'Womble'),
 
 	/*
 	|--------------------------------------------------------------------------
---
+++ app/views/emails/group/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h2>Thanks for booking to come to Womble 2013</h2>
+
+		<p>We’re dead chuffed that you’ve chosen to come to our event and were going to do everything we can do to make it the best Womble ever.</p>
+
+		<p>We’ve booked your activities for you and will be in contact in the coming months to ask you a few additional questions that will help the event run even more smoothly, but if you have any questions please drop us an email and we’ll pass it onto the team lead.</p>
+
+		<p>Now for the money side of things... Please send us a cheque for £20 per person attending; including leaders and adults to us at: Womble 2013 bookings, County Scout Centre, Winchester Road, Blaby, Leicestershire LE8 4H.  Cheques need to be payable to "Leicestershire Scout Council" and if you can write on the back the names of the people who you are paying for it makes it a lot easier for us.  Deposits are non refundable.</p>
+
+		<p>We’ll then be asking for the remaining £25 later this year (mid May) along with your health forms.</p>
+
+		<p>Finally if you have been to Womble before you’ll know that you need to be building up your arm muscles, doing chin ups and increasing your flexibility as much as you can do.</p>
+
+		<p>All the best,<br>
+			Womble</p>
+	</body>
+</html>
